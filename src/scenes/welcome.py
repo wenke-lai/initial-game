9d91c954fa8a86614b2baa4d9114fa0f09e30f4a
wenke.lai <wenke.studio@gmail.com>
2024-08-31 02:26:33 +0800

    fix: use pygame_gui instead of debug-overlay

--- src/scenes/welcome.py
+++ src/scenes/welcome.py
@@ -3,41 +3,43 @@ import pygame_gui as gui
 
 from src.components.scene import BaseScene
 
-from .ui import DebugOverlay
-
 
 class Welcome(BaseScene):
     def __init__(self):
         super().__init__()
 
-        self.ui = DebugOverlay()
-
-        # setup gui
-        self.gui_manager = gui.UIManager((self.width, self.height))
-        self.btn_start = gui.elements.UIButton(
-            relative_rect=pygame.Rect(self.width // 2, self.height // 2, 100, 50),
-            text="Start",
-            manager=self.gui_manager,
-        )
-
         # event status
         self.finished = False
 
+        # setup gui
+        self.gui_manager = gui.UIManager((self.width, self.height))
+        self.create_ui()
+
+    def create_ui(self):
+        gui.elements.UIButton(
+            relative_rect=pygame.Rect(0, 0, 100, 50),
+            text="Start",
+            manager=self.gui_manager,
+            anchors={"center": "center"},
+            command=self._start_the_game,
+        )
+        gui.elements.UILabel(
+            relative_rect=pygame.Rect(0, -60, 500, 500),
+            text="Welcome to the game",
+            manager=self.gui_manager,
+            anchors={"center": "center"},
+        )
+
+    def _start_the_game(self):
+        self.finished = True
+
     def process_events(self, event: pygame.event.Event):
         self.gui_manager.process_events(event)
-        if self.btn_start.check_pressed():
-            self.finished = True
 
     def run(self) -> str | None:
         super().run()
 
         self.gui_manager.update(pygame.time.get_ticks())
-        self.ui.update()
-        self.ui.append("Welcome to the game")
-        self.ui.append("Welcome to the game", "red")
-        self.ui.append("Welcome to the game", "green")
-
-        self.ui.draw()
         self.gui_manager.draw_ui(self.display_surface)
 
         if self.finished:
