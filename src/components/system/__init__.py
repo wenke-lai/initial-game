bb9580c14e62275ecfc78a00f64f6d74482a8037
wenke.lai <wenke.studio@gmail.com>
2024-09-04 02:14:30 +0800

    feat: add the move system

--- src/components/system/__init__.py
+++ src/components/system/__init__.py
@@ -1,5 +1,8 @@
 from .attack_system import AttackSystem
+from .move_system import ArrowMoveSystem, WASDMoveSystem
 
 __all__ = [
     "AttackSystem",
+    "ArrowMoveSystem",
+    "WASDMoveSystem",
 ]
---
+++ src/components/system/move_system.py
@@ -0,0 +1,72 @@
+import pygame
+
+
+class MoveSystem:
+    def __init__(
+        self,
+        sprite: pygame.sprite.Sprite,
+        collision_sprites: pygame.sprite.Group = None,
+    ):
+        self.sprite = sprite
+        self.vector = pygame.math.Vector2()
+        self.collision_sprites = collision_sprites
+
+    def input(self):
+        raise NotImplementedError("This method must be implemented in a subclass")
+
+    def move(self):
+        if self.vector.magnitude() == 0:
+            return
+
+        preview_pos = pygame.math.Vector2(self.sprite.hitbox.topleft)
+        move_vector = pygame.math.Vector2(self.vector.normalize() * self.sprite.speed)
+        self.sprite.hitbox.topleft += move_vector
+        self.collisions(preview_pos)
+        self.sprite.rect.center = self.sprite.hitbox.center
+
+    def collisions(self, preview_pos: pygame.math.Vector2):
+        for sprite in self.collision_sprites:
+            if sprite.hitbox.colliderect(self.sprite.hitbox):
+                if self.vector.x != 0:
+                    self.sprite.hitbox.x = preview_pos.x
+                if self.vector.y != 0:
+                    self.sprite.hitbox.y = preview_pos.y
+
+    def update(self):
+        self.input()
+        self.move()
+
+
+class ArrowMoveSystem(MoveSystem):
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.vector.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.vector.y = 1
+        else:
+            self.vector.y = 0
+        if keys[pygame.K_LEFT]:
+            self.vector.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.vector.x = 1
+        else:
+            self.vector.x = 0
+
+
+class WASDMoveSystem(MoveSystem):
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.vector.y = -1
+        elif keys[pygame.K_s]:
+            self.vector.y = 1
+        else:
+            self.vector.y = 0
+        if keys[pygame.K_a]:
+            self.vector.x = -1
+        elif keys[pygame.K_d]:
+            self.vector.x = 1
+        else:
+            self.vector.x = 0
+
